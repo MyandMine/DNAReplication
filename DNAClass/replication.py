14337e6774d297374a905804161c14b844f35a88
lincaho <lincaho.hide@gmail.com>
2016-12-14 11:14:53 +0800

    完成了第二课所有内容

--- DNAClass/replication.py
+++ DNAClass/replication.py
@@ -1,8 +1,31 @@
+# Input:  Strings Pattern and Text, and an integer d
+# Output: The number of times Pattern appears in Text with at most d mismatches
+def ApproximatePatternCount(Pattern, Text, d):
+    count = 0 # initialize count variable
+    # your code here
+    count = len(ApproximatePatternMatching(Pattern,Text,d))
+    return count
+
+# Input:  Strings Pattern and Text along with an integer d
+# Output: A list containing all starting positions where Pattern appears
+# as a substring of Text with at most d mismatches
+def ApproximatePatternMatching(Pattern, Text, d):
+    positions = [] # initializing list of positions
+    # your code here
+    for i in range(len(Text) - len(Pattern) +1 ):
+        if HammingDistance(Text[i:i+len(Pattern)],Pattern) <= d:
+            positions.append(i)
+    return positions
+
 # Input:  Two strings p and q
 # Output: An integer value representing the Hamming Distance between p and q.
 def HammingDistance(p, q):
     # your code here
-    
+    count = 0
+    for i in range(len(p)):
+        if p[i]!=q[i]:
+            count = count + 1
+    return count
 # Input:  A DNA string Genome
 # Output: A list containing all integers i minimizing Skew(Prefix_i(Text)) over all values of i (from 0 to |Genome|)
 def MinimumSkew(Genome):
@@ -87,4 +110,8 @@ def PatternMatching(Pattern, Genome):
 ### DO NOT MODIFY THE CODE BELOW THIS LINE ###
 import sys
 #lines = sys.stdin.read().splitlines()
-print(MinimumSkew("TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT"))
+print(FrequentWords("CGGAGGACTCTAGGTAACGCTTATCAGGTCCATAGGACATTCA",3))
+a=list(range(5))
+b=a
+a[2]=12
+print("sdfg")
